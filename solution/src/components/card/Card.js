6c8ec97ed4211c187497ddea94cf03b78d01e8b3
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-29 20:54:00 +0300

    card.js

--- solution/src/components/card/Card.js
+++ solution/src/components/card/Card.js
@@ -1,9 +1,16 @@
+import { languages } from "../../helpers/data";
+
 const Card = () => {
   return (
     <div className="cards-area-container">
       <div className="bars"></div>
       <h1 className="language-title">Languages</h1>
-      <div className="cards-container">cards container</div>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          //   return <h4>{item.name}</h4>;
+          return <Item card={item} key={index} />;
+        })}
+      </div>
     </div>
   );
 };
