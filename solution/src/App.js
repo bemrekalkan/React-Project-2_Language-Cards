115e2950b6c34173d5be54520ad7be0fbef83f8f
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-29 20:45:49 +0300

    keep going

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Card from "./components/card/Card";
 import Header from "./components/header/Header";
 function App() {
   return (
     <div className="main-container">
       <Header />
+      <Card />
     </div>
   );
 }
--- solution/src/components/card/Card.js
+++ solution/src/components/card/Card.js
@@ -1,5 +1,11 @@
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h1 className="language-title">Languages</h1>
+      <div className="cards-container">cards container</div>
+    </div>
+  );
 };
 
 export default Card;
