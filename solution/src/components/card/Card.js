98f67cf7a6da3566d2367b17c81f3034ad72b33a
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-29 20:58:59 +0300

    data.js updated

--- solution/src/components/card/Card.js
+++ solution/src/components/card/Card.js
@@ -7,7 +7,6 @@ const Card = () => {
       <h1 className="language-title">Languages</h1>
       <div className="cards-container">
         {languages.map((item, index) => {
-          //   return <h4>{item.name}</h4>;
           return <Item card={item} key={index} />;
         })}
       </div>
